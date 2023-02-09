1d16ef8e6ce6d623f287e115b85f7214b22a72fe
victor <victor.ernesto.z97@gmail.com>
2023-02-08 21:48:16 -0600

    ejercicio 18

---
+++ elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,95 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map( card => card * 2)
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  for (let index = 0; index < deck.length; index++) {
+    if (deck[index] === 3){
+      deck.splice(index,0,3,3)
+      index = index+3
+    }
+  }
+  return deck
+}
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  return deck.slice(4,6)
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  let first = deck.splice(0,1)
+  let last = deck.splice(-1)
+  deck.splice( deck.length/2 , 0, ...last, ...first)
+  return deck
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  for (let i = 0; i < deck.length; i++) {
+    if( deck[i] !== 2 ){
+      deck.splice(i,1)
+      i--
+    }
+  }
+  return deck
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  return deck.sort((num1, num2) => num1 - num2 )
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse();
+}
